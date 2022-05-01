9a2e57a1a6ebe342e16ccbccbb23c24452064f37
Mustafa Burak Kilic <burak.kilic@mobilistbilisim.com>
2022-05-01 22:34:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definition with arithmetic operators
+            int x=3;
+            
+            x+=2; //x=x+2;
+            x-=2; //x=x-2;
+            x/=2; //x=x/2;
+            x*=2; //x=x*2;
+
+            //Logic Operators
+            bool yes=true;
+            bool no=false;
+
+            if (yes && no)
+            {
+                Console.WriteLine("yes and no");
+            }
+
+            else if (yes || no)
+            {
+                Console.WriteLine("yes or no");
+            }
+
+            else if (!yes && !no)
+            {
+                Console.WriteLine( "not yes and not no"  );
+            }
+
+            else if (!yes || !no)
+            {
+                Console.WriteLine("not yes or not no");
+            }
+
+            //İlişkisel operatörler
+            //<,>,<=,>=,==,!=
+            int a=3;
+            int b=4;
+            bool sonuc = a<b; //false
+            sonuc= a>b; //true
+            sonuc= a<=b; //false
+            sonuc= a>=b; //true
+            sonuc= a==b; //false
+            sonuc= a!=b; //true
+        }
+    }
+}
